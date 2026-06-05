f58ba93ffee55a1adaba69b0bec6a674b3de5bc0
Abdalla Elsiddig <bh22500263@utb.edu.bh>
2026-06-05 14:44:09 +0300

    updating the names

--- app/page.jsx
+++ app/page.jsx
@@ -112,8 +112,9 @@ const Portfolio = () => {
   ];
 
   const certificateFiles = [
-    { title: ' Hardware_and_Upgrade_Support ', file: 'Hardware_and_Upgrade_Support.pdf', type: 'pdf' },
-    { title: ' Getting_Started_with_Cisco_Packet_Tracer_certificate_ ', file: 'Getting_Started_with_Cisco_Packet_Tracer_certificate_.pdf', type: 'pdf' },
+    { title: ' Hardware and Upgrade Support ', file: 'Hardware and Upgrade Support.pdf', type: 'pdf' },
+    { title: ' Getting Started with Cisco Packet Tracer ', file: 'Getting Started with Cisco Packet Tracer.pdf', type: 'pdf' },
+    { title: 'Networking', file: 'Networking.pdf', type: 'pdf' },
     { title: 'IP Network Foundamental', file: 'IP Network Foundamental.pdf', type: 'pdf' },
     { title: '1 MILLION PROMPTERS', file: '1 MILLION PROMPTERS.pdf', type: 'pdf' },
     { title: 'First Aid Training', file: 'First Aid Training.pdf', type: 'pdf' },
